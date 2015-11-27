9c5726925e8a4a20266cc93a5439a6eb8e8d2d2d
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-27 06:49:24 -0500

    first commit.

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * to-choices <https://github.com/jonschlinkert/to-choices>
+ *
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
