72e1c98e118b4979654d3fcceb6a3f1734cbbb56
jonschlinkert <jon.schlinkert@sellside.com>
2016-02-27 10:41:32 -0500

    run update

--- test.js
+++ test.js
@@ -4,18 +4,18 @@ require('mocha');
 var assert = require('assert');
 var toChoices = require('./');
 
-describe('to-choices', function () {
-  it('should support the name as a string', function () {
+describe('to-choices', function() {
+  it('should support the name as a string', function() {
     var question = toChoices('foo', ['a', 'b', 'c']);
     assert.equal(question.name, 'foo');
   });
 
-  it('should support the name as an object property', function () {
+  it('should support the name as an object property', function() {
     var question = toChoices({name: 'foo', choices: ['a', 'b', 'c']});
     assert.equal(question.name, 'foo');
   });
 
-  it('should support choices as an object property', function () {
+  it('should support choices as an object property', function() {
     var question = toChoices({name: 'foo', choices: ['a', 'b', 'c']});
     assert.deepEqual(question.choices, [
       { name: 'all', value: [ 'a', 'b', 'c' ] },
@@ -26,7 +26,7 @@ describe('to-choices', function () {
     ]);
   });
 
-  it('should support choices as an array', function () {
+  it('should support choices as an array', function() {
     var question = toChoices('foo', ['a', 'b', 'c']);
     assert.deepEqual(question.choices, [
       { name: 'all', value: [ 'a', 'b', 'c' ] },
